2316d2cf66a9bbad59f04b385c43d48ff3b78ca4
Greta Gri <greta.klimaite@gmail.com>
2018-04-10 17:58:13 +0300

    Created app with several initial layouts.

---
+++ app/src/main/java/com/example/android/vilniusguide/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.vilniusguide;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+    }
+}
